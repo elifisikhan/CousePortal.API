46afb11eb9f5d86644c22e59030e774c1f928580
Elif Işıkhan <isikhanelif@gmail.com>
2020-11-09 13:41:53 +0300

    Initial commit

---
+++ Libraries/Portal.Domain/Common/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public int CreatedBy { get; set; }
+        public short IsActive { get; set; } = 1;
+        public DateTime CreatedTime { get; set; } = DateTime.Now;
+        public DateTime? UpdatedTime { get; set; }
+        public int? UpdatedBy { get; set; }        
+    }
+}
---
+++ Libraries/Portal.Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using Portal.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Domain.Entities
+{
+    public class User:BaseEntity
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
