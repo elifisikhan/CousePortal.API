9edf032478af16950359bcf932d8e2f1d77f76f7
Elif Işıkhan <isikhanelif@gmail.com>
2020-11-16 12:27:16 +0300

    postgresql vt oluşturuldu

---
+++ Libraries/Portal.Application/System/ServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Portal.Infrastructure.Persistence;
+using Portal.Infrastructure.Persistence.Ef;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Portal.Application.System
+{
+
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection InjectApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>)); // AddScoped : Farklı isteklerde farklı instanceler oluşturur. Yani bir istekte oluşturulup dispose edilir.
+            return services;
+        }
+
+        public static IServiceCollection AddCustomizedDataStore(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<PortalDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("Portal.Infrastructure")));
+
+            services.AddScoped<DbContext>(provider => provider.GetService<PortalDbContext>());
+            return services;
+        }
+        
+    }
+}
--- Libraries/Portal.Domain/Entities/Media.cs
+++ Libraries/Portal.Domain/Entities/Media.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Windows.Networking.NetworkOperators;
 
 namespace Portal.Domain.Entities
 {
